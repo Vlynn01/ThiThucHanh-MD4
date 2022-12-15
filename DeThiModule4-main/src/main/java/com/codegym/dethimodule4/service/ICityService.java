752bc70959046879d77dfe56d8d2455d1350b5a9
Vlynn Noo <89231237+Vlynn01@users.noreply.github.com>
2022-12-15 10:04:17 +0700

    Delete DeThiModule4-main directory

--- DeThiModule4-main/src/main/java/com/codegym/dethimodule4/service/ICityService.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.dethimodule4.service;
-
-import com.codegym.dethimodule4.model.City;
-
-public interface ICityService extends IGeneralService<City> {
-    City findCityByName(String cityName);
-}
--- DeThiModule4-main/src/main/java/com/codegym/dethimodule4/service/ICountryService.java
+++
@@ -1,6 +0,0 @@
-package com.codegym.dethimodule4.service;
-
-import com.codegym.dethimodule4.model.Country;
-
-public interface ICountryService extends IGeneralService<Country>  {
-}
